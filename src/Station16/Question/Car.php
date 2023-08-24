4ca1f12f46fb7e6b1a8b187843bff6955cba9958
知念英弥 <s19013@std.it-college.ac.jp>
2023-08-24 12:03:11 +0900

    q16

--- src/Station16/Question/Car.php
+++ src/Station16/Question/Car.php
@@ -1,3 +1,16 @@
 <?php
 
 namespace Src\Station16\Question;
+
+class Car {
+    public string $name = '';
+    public int $passenger = 0;
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function run() : void{ echo "走行する"; }
+
+    public function pickup(int $passenger) : int{ return $this->passenger += $passenger; }
+}
--- src/Station16/Question/Question.php
+++ src/Station16/Question/Question.php
@@ -6,6 +6,12 @@ class Question
 {
     public function main(string $humanName, string $carName, int $passengers): void
     {
+        $human = new Human($humanName);
+        $car = new Car($carName);
+
+        $human->buyCar($car);
+        $car->run();
+        echo $car->pickup($passengers);
     }
 }
 
